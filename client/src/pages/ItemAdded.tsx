fa5502a679cae2b14aefaf46e2892f26d1b8f45e
Vorleak Yek <vorleakyek@gmail.com>
2024-04-12 06:53:18 +0000

    update some styling

--- client/src/pages/ItemAdded.tsx
+++ client/src/pages/ItemAdded.tsx
@@ -1,14 +1,18 @@
 export default function ItemAdded() {
   return (
     <>
-      <div>
-        <div>
+      <div className="border-solid border-black border max-w-5xl">
+        <div className="text-left p-3 text-xl">
           <p>Item added to cart</p>
         </div>
-        <div>
-          <img src="" alt="parrots" />
-          <p>Lego Creator 3 in 1</p>
-          <p>Quantity: 2</p>
+        <div className="flex justify-center py-3 px-10 ">
+          <div className="basis-1/2">
+            <img src="../images/HarvestMoon.jpg" alt="parrots" className="" />
+          </div>
+          <div className="basis-1/2 pt-5">
+            <p className=" text-xl font-semibold mb-5">Lego Creator 3 in 1</p>
+            <p className="text-base text-slate-500">Quantity: 2</p>
+          </div>
         </div>
         <div>
           <button>Continue Shopping</button>
