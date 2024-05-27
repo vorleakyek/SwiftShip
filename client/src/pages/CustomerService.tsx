c2cb62358e44b3cf1281effa40cb34cc1894b36f
Vorleak Yek <vorleakyek@gmail.com>
2024-05-27 05:31:03 +0000

    update the display of customer service page

--- client/src/pages/CustomerService.tsx
+++ client/src/pages/CustomerService.tsx
@@ -1,19 +1,26 @@
 export default function CustomerService() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
   return (
-    <div>
-      <h1>Customer Service</h1>
-      <p>
-        Thank you for contacting us, please call us at ____ leave as message
-        below.
-      </p>
-      <textarea
-        className="border border-slate-500"
-        name=""
-        id=""
-        cols="50"
-        rows="10"></textarea>
+    <div className="max-w-5xl py-3">
+      <form onSubmit={handleSubmit}>
+        <h1 className="font-semibold">Customer Service</h1>
+        <p>
+          Thank you for contacting us, please leave a message below with your
+          email address or your phone number for us to contact you back.
+        </p>
+        <div className="flex justify-center py-3">
+          <textarea
+            className="border border-slate-500 block rounded-lg"
+            name=""
+            id=""
+            cols={50}
+            rows={7}></textarea>
+        </div>
 
-      <button>Send</button>
+        <button className="bg-amber-400 rounded-3xl px-5 py-1">Send</button>
+      </form>
     </div>
   );
 }
